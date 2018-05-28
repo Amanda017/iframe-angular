a6fd699516272c3948404b906efd5769f17b7e5c
p-admin <piterpan.kks@gmail.com>
2018-05-28 11:13:05 -0700

    piterpan edits

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,14 @@
+// piterpan
+$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+
+
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
